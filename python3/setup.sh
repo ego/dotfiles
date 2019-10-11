a8febb315118b102fdf02d7e9e142e35be131fe5
0x7c48 <0x7c48@gmail.com>
2019-10-11 11:39:18 +0300

    python requirements

--- python3/setup.sh
+++ python3/setup.sh
@@ -14,16 +14,11 @@ pip3 install --upgrade setuptools
 pip3 install --upgrade pip
 echo "$(pip --version)"
 
-pip3 install virtualenv
+pip3 install -r -U ./requirements.txt
+
 echo "$(virtualenv --version)"
-
-pip3 install 'ipython[zmq,qtconsole,notebook,test]'
 echo "$(ipython --version)"
-
-pip3 install aws-shell
 echo "$(aws-shell --version)"
 
-pip3 install autopep8 pep8 pylint
-
-success "Successfully installed emacs."
+success "Successfully installed python3."
 exit
